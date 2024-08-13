d370ab5543049811f759a094120f885e18806ab2
Princep27 <107925131+Princep27@users.noreply.github.com>
2024-08-13 20:13:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Banner from './Banner';
 import Dashboard from './Dashboard';
 
