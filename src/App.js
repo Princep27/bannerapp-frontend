f2ec6a3388bcc67fcb43134ab81d372885dc5a96
Princep27 <107925131+Princep27@users.noreply.github.com>
2024-08-13 20:21:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Banner from './Banner';
 import Dashboard from './Dashboard';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Banner} />
-        <Route path="/dashboard" component={Dashboard} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Banner />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </Router>
   );
 }
